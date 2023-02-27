60a7d08afdbf2c8f17119844a7312e4726acedb9
Isaiah <67047480+isaiahaquino@users.noreply.github.com>
2023-02-26 18:40:34 -0800

    working state management

--- redux/store.ts
+++ redux/store.ts
@@ -1,14 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cartReducer } from './cartSlice';
 import { quickReducer } from './quickSlice';
+import { loadState, saveState } from './stateManage';
+
 
 const reducer = {
   cart: cartReducer,
   quick: quickReducer
 };
 
+const preloadedState = loadState()
+
 const store = configureStore({
   reducer,
+  preloadedState,
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export default store;
